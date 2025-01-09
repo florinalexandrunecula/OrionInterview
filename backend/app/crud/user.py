dc4fb2d5d44275497095e5f37053bfdeffbf9cba
Alexandru Necula <alex_necula99@yahoo.com>
2025-01-09 13:23:20 +0200

    Modified project structure; implemented admin management page

--- backend/app/crud/user.py
+++ backend/app/crud/user.py
@@ -7,6 +7,20 @@ def get_user(db: Session, username: str):
     return db.query(User).filter(User.username == username).first()
 
 
+def get_users(db: Session):
+    return db.query(User).all()
+
+
+def delete_user(db: Session, user: User):
+    try:
+        db.delete(user)
+        db.commit()
+    except Exception as e:
+        print("Caught following exception: {e}")
+        return False
+    return True
+
+
 def create_user(db: Session, username: str, password: str):
     hashed_password = get_password_hash(password)
     db_user = User(username=username, hashed_password=hashed_password)
